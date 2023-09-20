d0812c7bd3a19a5504732fcbe81421c4a4e4e6f0
Sheldon John Van Zyl <123169408+SheldonVZyl@users.noreply.github.com>
2023-09-20 08:24:58 +0200

    Add files via upload

---
+++ chap2/index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'contact.html'));
+});
+
+app.listen(3000, () => {
+    console.log("App listening on port 3000");
+});
